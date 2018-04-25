d7a64eac08274197ffa50c7e6ec78a10c20ceca8
Oriol Fornés <oriol@cmmt.ubc.ca>
2018-04-25 08:54:05 -0700

    updated unirefdb names

--- msa/sh/uniref.sh
+++ msa/sh/uniref.sh
@@ -2,14 +2,13 @@ mkdir tmp
 curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref50/uniref50.fasta.gz
 gunzip uniref50.fasta.gz
 mmseqs createdb uniref50.fasta uniref50
-mmseqs createindex uniref50 tmp
+mmseqs createindex uniref50.db tmp
 curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz
 gunzip uniref90.fasta.gz
 mmseqs createdb uniref90.fasta uniref90
-mmseqs createindex uniref90 tmp
+mmseqs createindex uniref90.db tmp
 curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref100/uniref100.fasta.gz
 gunzip uniref100.fasta.gz
 mmseqs createdb uniref100.fasta uniref100
-mmseqs createindex uniref100 tmp
+mmseqs createindex uniref100.db tmp
 rm -rf tmp
-
