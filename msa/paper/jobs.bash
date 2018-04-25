7d70b49260f26975ebc330078092aa4b88423b11
Oriol Fornés <oriol@cmmt.ubc.ca>
2018-04-25 14:08:45 -0700

    updated jobs.bash

--- msa/paper/jobs.bash
+++ msa/paper/jobs.bash
@@ -8,5 +8,5 @@ echo "#SBATCH --mem=500GB" >> buildmsa.pbs
 echo "#SBATCH --nodes=1" >> buildmsa.pbs
 echo "#SBATCH --ntasks=32" >> buildmsa.pbs
 echo "#SBATCH --output=buildmsa.log" >> buildmsa.pbs
-echo "#SBATCH --time=7-00:00" >> buildmsa.pbs
+echo "#SBATCH --time=168:00:00" >> buildmsa.pbs
 ls *.fa | perl -e '$job=1;while(<>){chomp;$header=substr($_,0,5);system("echo \"python /home/ofornes/RADI/msa/buildmsa.py --dummy=/home/ofornes/scratch/RADI/paper/$header/tmp/ -i /home/ofornes/RADI/msa/paper/$header.fa -o /home/ofornes/scratch/RADI/paper/$header/\" >> buildmsa.pbs");$job++;}'
